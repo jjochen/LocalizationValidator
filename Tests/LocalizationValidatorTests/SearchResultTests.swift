51327dcbf124aafa244594c6cc208b68d5d45602
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2020-08-31 10:32:02 +0200

    remove empty lines

--- Tests/LocalizationValidatorTests/SearchResultTests.swift
+++ Tests/LocalizationValidatorTests/SearchResultTests.swift
@@ -4,14 +4,10 @@ import XCTest
 
 final class SearchResultTests: XCTestCase {
     func testInitializer() {
-
-
         let searchResult = SearchResult(filePath: "filePath", position: FilePosition(lineNumber: 3, positionInLine: 4), key: "key")
         XCTAssertEqual(searchResult.filePath, "filePath")
         XCTAssertEqual(searchResult.position.lineNumber, 3)
         XCTAssertEqual(searchResult.position.positionInLine, 4)
         XCTAssertEqual(searchResult.key, "key")
     }
-
-
 }
